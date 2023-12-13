3ba35efa935a9bb07dd9a000f7342bb0a760fc17
lindsaySchool <r0883883@student.thomasmore.be>
2023-12-13 13:56:58 +0100

    feat: style shop component

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -6,13 +6,14 @@ import { ref,defineEmits } from 'vue';
 <template>
 <!--An overview of the shoes to buy-->
 <div class="shop">
-  <h1>Shop</h1>
+  <h1 class="shop__title">Shop</h1>
   <div class="shop__container">
     <div class="shop__container__item">
-      <img src="../assets/cover_shoeModel.png" alt="air-rev-nitro-s-triple-white">
-      <h2>AIR REV. NITRO S TRIPLE WHITE</h2>
-      <p>€51.99</p>
-      <button>View</button>
+        <router-link to="/product-detail">
+          <img class="shop__item-image" src="../assets/cover_shoeModel.png" alt="air-rev-nitro-s-triple-white">
+        </router-link>
+      <h2  class="shop__item-title">AIR REV. NITRO S TRIPLE WHITE</h2>
+      <p class="shop__item-price">€51.99</p>
     </div>
 </div>
 </div>
@@ -20,4 +21,47 @@ import { ref,defineEmits } from 'vue';
 </template>
 
 <style scoped>
+.shop {
+  font-family: Arial, sans-serif;
+  color: #333;
+}
+
+.shop__title {
+  font-size: 2em;
+  margin-bottom: 1em;
+  padding: 0 2em;
+}
+
+.shop__container {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin-left: 2em;
+}
+
+.shop__item {
+  width: 200px;
+  margin: 1em;
+  padding: 1em;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.shop__item-image {
+  width: 80%;
+  height: auto;
+  display: inline-block;
+  margin: 0 auto;
+}
+
+.shop__item-title {
+  font-size: 1.2em;
+  margin: 0.5em 0;
+}
+
+.shop__item-price {
+  color: #999;
+  margin-bottom: 1em;
+}
 </style>
