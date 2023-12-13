5ad94b9a66be2c7579fc63ed91f8e1468bace144
lindsaySchool <r0883883@student.thomasmore.be>
2023-12-13 13:37:28 +0100

    fix: default router

--- src/main.js
+++ src/main.js
@@ -3,4 +3,23 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//import VueRouter
+import * as VueRouter from 'vue-router'
+
+//import components
+import Configurator from './components/Configurator.vue'
+
+//define routes
+const routes = [
+  { path: '/', component: Configurator }
+]
+
+const router = VueRouter.createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: VueRouter.createWebHashHistory(),
+    routes, // short for `routes: routes`
+});
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
